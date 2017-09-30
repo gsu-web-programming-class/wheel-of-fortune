3ee1ce1a39b4c2fc665d94db8a55b9f89caaa908
Joshua King <joshua@antechdevelopment.com>
2017-09-30 17:51:20 -0400

    Removed useless php functions in utils class

--- php/php_utils.php
+++ php/php_utils.php
@@ -1,14 +1,6 @@
 <?php
 	//    FUNCTIONS
 
-	function calcAreaOfCircle( $radius ) {
-		return pi() * $radius * $radius;
-	}
-
-	function fahrenheitToCelsius( $fahrenheit ) {
-		return ( 5 / 9 ) * ( $fahrenheit - 32 );
-	}
-
 	function charAfterFirstOccurrence( $str, $searchStr ) {
 		$index = strpos( $str, $searchStr );
 		if ( $index ) {
@@ -16,10 +8,6 @@
 		} else {
 			return "[NOT FOUND]";
 		}
-	}
-
-	function isPalindrome( $str ) {
-		return substr_compare( $str, join( array_reverse( str_split( $str ) ) ), 0, strlen( $str ), true ) == 0;
 	}
 
 	function echoIf( $bool, $strTrue, $strFalse = false ) {
@@ -32,14 +20,6 @@
 
 	function isEven( $num ) {
 		return $num % 2 == 0;
-	}
-
-	function getFib( $n ) {
-		return round( pow( ( sqrt( 5 ) + 1 ) / 2, $n ) / sqrt( 5 ) );
-	}
-
-	function isPrime( $num ) {
-		return ( ( pow( 2, ( $num - 1 ) ) ) % $num == 1 );
 	}
 
 	function getUsernameFromEmail( $email ) {
