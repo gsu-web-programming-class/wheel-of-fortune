bdff70b15fcc31a0a4a650877afdc7680752138f
Joshua King <joshua@antechdevelopment.com>
2017-09-30 17:49:17 -0400

    Fleshed out the structure of the project a little further. Added scss and php files/directories

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/**
-	 * Created by PhpStorm.
-	 * User: joshuaking
-	 * Date: 9/30/17
-	 * Time: 16:09
-	 */
-
-	echo "Hello world!";
---
+++ php/user_guess_processor.php
@@ -0,0 +1,8 @@
+<?php
+	/*
+	 * Hamel, use this as a starting point for Github issues #4 and #5.
+	 * Feel free to create other files, this is just to help us on organization this time
+	 *
+	 * See https://github.com/gsu-web-programming-class/wheel-of-fortune/issues/4
+	 * See https://github.com/gsu-web-programming-class/wheel-of-fortune/issues/5
+	 */
