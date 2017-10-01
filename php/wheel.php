7a4525654b6e5ccc5b53fbf5b6ea0762ac1b1de2
Joshua King <joshua@antechdevelopment.com>
2017-10-01 01:33:36 -0400

    #2 - Wheel is now round

--- php/wheel.php
+++ php/wheel.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Wheel of Fortune</title>
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="../main.css">
     </head>
     <body>
         <div id="wheel">
@@ -19,23 +19,23 @@
 						4  => [ "value" => 350, "display" => "$350" ],
 						5  => [ "value" => 400, "display" => "$400" ],
 						6  => [ "value" => 400, "display" => "$400" ],
-						7  => [ "value" => 450, "display" => "$450" ],
+						7  => [ "value" => 400, "display" => "$400" ],
 						8  => [ "value" => 450, "display" => "$450" ],
-						9  => [ "value" => 500, "display" => "$500" ],
+						9  => [ "value" => 450, "display" => "$450" ],
 						10 => [ "value" => 500, "display" => "$500" ],
-						11 => [ "value" => 550, "display" => "$550" ],
-						12 => [ "value" => 600, "display" => "$600" ],
+						11 => [ "value" => 500, "display" => "$500" ],
+						12 => [ "value" => 550, "display" => "$550" ],
 						13 => [ "value" => 600, "display" => "$600" ],
-						14 => [ "value" => 700, "display" => "$700" ],
+						14 => [ "value" => 600, "display" => "$600" ],
 						15 => [ "value" => 700, "display" => "$700" ],
-						16 => [ "value" => 750, "display" => "$750" ],
-						17 => [ "value" => 800, "display" => "$800" ],
+						16 => [ "value" => 700, "display" => "$700" ],
+						17 => [ "value" => 750, "display" => "$750" ],
 						18 => [ "value" => 800, "display" => "$800" ],
-						19 => [ "value" => 900, "display" => "$900" ],
-						20 => [ "value" => 900, "display" => "$900" ],
-						21 => [ "value" => 5000, "display" => "$5000" ],
-						22 => [ "value" => 0, "display" => "Bankrupt" ],
-						23 => [ "value" => 0, "display" => "Bankrupt" ],
+						19 => [ "value" => 800, "display" => "$800" ],
+						20 => [ "value" => 850, "display" => "$800" ],
+						21 => [ "value" => 900, "display" => "$900" ],
+						22 => [ "value" => 900, "display" => "$900" ],
+						23 => [ "value" => 5000, "display" => "$5000" ],
 					];
 					shuffle( $money_amounts );
 					for ( $i = 0; $i < 24; $i ++ ) {
