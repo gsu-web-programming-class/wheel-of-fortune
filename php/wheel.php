9de554632ce45cd3e0313a8b0c286cd6eb2b55da
Joshua King <joshua@antechdevelopment.com>
2017-10-03 11:19:30 -0400

    Finished wheel

--- php/wheel.php
+++ php/wheel.php
@@ -9,63 +9,68 @@
     </head>
     <body>
         <?php echo play_sound( WHEEL_SPIN, false, "../media/audio" ); ?>
-        <div id="wheel">
-            <ul class='pie'>
-                <?php
-                    $money_amounts = [
-                        0  => [ "value" => 300, "display" => "$300" ],
-                        1  => [ "value" => 300, "display" => "$300" ],
-                        2  => [ "value" => 300, "display" => "$300" ],
-                        3  => [ "value" => 350, "display" => "$350" ],
-                        4  => [ "value" => 350, "display" => "$350" ],
-                        5  => [ "value" => 400, "display" => "$400" ],
-                        6  => [ "value" => 400, "display" => "$400" ],
-                        7  => [ "value" => 400, "display" => "$400" ],
-                        8  => [ "value" => 450, "display" => "$450" ],
-                        9  => [ "value" => 450, "display" => "$450" ],
-                        10 => [ "value" => 500, "display" => "$500" ],
-                        11 => [ "value" => 500, "display" => "$500" ],
-                        12 => [ "value" => 550, "display" => "$550" ],
-                        13 => [ "value" => 600, "display" => "$600" ],
-                        14 => [ "value" => 600, "display" => "$600" ],
-                        15 => [ "value" => 700, "display" => "$700" ],
-                        16 => [ "value" => 700, "display" => "$700" ],
-                        17 => [ "value" => 750, "display" => "$750" ],
-                        18 => [ "value" => 800, "display" => "$800" ],
-                        19 => [ "value" => 800, "display" => "$800" ],
-                        20 => [ "value" => 850, "display" => "$800" ],
-                        21 => [ "value" => 900, "display" => "$900" ],
-                        22 => [ "value" => 900, "display" => "$900" ],
-                        23 => [ "value" => 5000, "display" => "$5000" ],
-                    ];
-                    shuffle( $money_amounts );
-                    for ( $i = 0; $i < 24; $i++ ) {
-                        echo "<li class=\"slice\"><div class=\"slice-contents\"><span>";
-                        echo $money_amounts[ $i ][ "display" ];
-                        echo "</span></div></li>";
-                    }
-                ?>
-                <style type="text/css" rel="stylesheet">
-                    #wheel {
+        <div id="wheel-wrapper">
+            <h1 id="down-arrow">&#x21E9;</h1>
+            <div id="wheel">
+                <ul>
                     <?php
-                        $wheel_index      = rand( 0, count( $money_amounts ) );
-                        $wheel_anim = "wheel-rotation-$wheel_index";
-
-                        $prev_wheel_index = isset($_GET["prev_wheel_index"]) ? $_GET["prev_wheel_index"] : 0;
-
-                        echo "-webkit-animation-name : $wheel_anim;";
-                        echo "-o-animation-name : $wheel_anim;";
-                        echo "animation-name : $wheel_anim;";
-                        echo "transform : rotate(".($prev_wheel_index*(360/24))."deg);";
+                        $money_amounts = [
+                            0  => [ "value" => 300, "display" => "$300" ],
+                            1  => [ "value" => 300, "display" => "$300" ],
+                            2  => [ "value" => 300, "display" => "$300" ],
+                            3  => [ "value" => 350, "display" => "$350" ],
+                            4  => [ "value" => 350, "display" => "$350" ],
+                            5  => [ "value" => 400, "display" => "$400" ],
+                            6  => [ "value" => 400, "display" => "$400" ],
+                            7  => [ "value" => 400, "display" => "$400" ],
+                            8  => [ "value" => 450, "display" => "$450" ],
+                            9  => [ "value" => 450, "display" => "$450" ],
+                            10 => [ "value" => 500, "display" => "$500" ],
+                            11 => [ "value" => 500, "display" => "$500" ],
+                            12 => [ "value" => 550, "display" => "$550" ],
+                            13 => [ "value" => 600, "display" => "$600" ],
+                            14 => [ "value" => 600, "display" => "$600" ],
+                            15 => [ "value" => 700, "display" => "$700" ],
+                            16 => [ "value" => 700, "display" => "$700" ],
+                            17 => [ "value" => 750, "display" => "$750" ],
+                            18 => [ "value" => 800, "display" => "$800" ],
+                            19 => [ "value" => 800, "display" => "$800" ],
+                            20 => [ "value" => 850, "display" => "$800" ],
+                            21 => [ "value" => 900, "display" => "$900" ],
+                            22 => [ "value" => 900, "display" => "$900" ],
+                            23 => [ "value" => 5000, "display" => "$5000" ],
+                        ];
+                        shuffle( $money_amounts );
+                        for ( $i = 0; $i < 24; $i++ ) {
+                            echo "<li><div class=\"slice-contents\"><span>";
+                            echo $money_amounts[ $i ][ "display" ];
+                            echo "</span></div></li>";
+                        }
                     ?>
-                    }
-                </style>
-                <?php
-                    echo "<h1>$wheel_index</h1>";
-                    echo "<input hidden name=\"prev_wheel_index\" type=\"text\" value=\"$wheel_index\">";
-                ?>
-            </ul>
-            <img src="../media/img/wheel_of_fortune_wheel_center.png">
+                    <style type="text/css" rel="stylesheet">
+                        #wheel {
+                        <?php
+                            $numberOfSections = count( $money_amounts );
+                            $wheel_index      = rand( 0, $numberOfSections-1 );
+                            $wheel_anim = "wheel-rotation-$wheel_index";
+
+                            $prev_wheel_index = isset($_GET["prev_wheel_index"]) ? $_GET["prev_wheel_index"] : 0;
+
+                            echo "-webkit-animation-name : $wheel_anim;";
+                            echo "-o-animation-name : $wheel_anim;";
+                            echo "animation-name : $wheel_anim;";
+                            echo "transform : rotate(".($prev_wheel_index*(360/$numberOfSections))."deg);";
+                        ?>
+                        }
+                    </style>
+                </ul>
+                <img src="../media/img/wheel_of_fortune_wheel_center.png">
+            </div>
+            <?php
+                echo "<h1>$wheel_index</h1>";
+                echo "<h1>" . ( $money_amounts[ $wheel_index ][ "display" ] ) . "</h1>";
+                echo "<input hidden name=\"prev_wheel_index\" type=\"text\" value=\"$wheel_index\">";
+            ?>
         </div>
     </body>
 </html>
